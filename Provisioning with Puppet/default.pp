46b4462bae20105820ee9f98bf5f637be06f9ab2
Ahmed Ismail <54498216+A-Ismail-son@users.noreply.github.com>
2021-09-26 22:32:36 +0200

    Update default.pp

--- Provisioning with Puppet/default.pp
+++ Provisioning with Puppet/default.pp
@@ -1,1 +1,28 @@
 
+exec {'apt-get update':
+command => '/usr/bin/apt-get update'
+}
+
+
+package { 'node':
+require => Exec['install script'],
+ensure => installed,
+
+}
+
+package { 'curl':
+require => Exec['apt-get update'],
+ensure => 'installed 
+
+}
+
+exec { 'install script':
+command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_12.x | sudo -E b>
+}
+
+
+exec { 'build essential':
+    require => Exec['install script', 'apt-get update'],
+    command => '/usr/bin/sudo apt-get install -y build-essential'
+}
+
