f48bb7a5a8a4d0a63767b4a0122d56638f425422
Ahmed Ismail <54498216+A-Ismail-son@users.noreply.github.com>
2021-09-28 23:50:56 +0200

    Update default.pp

--- Basic Multi-Machine Setup/manifests/default.pp
+++ Basic Multi-Machine Setup/manifests/default.pp
@@ -1,1 +1,25 @@
 
+node 'appserver' {
+  include apt_update
+  include nodejs
+}
+
+node 'dbserver' {
+  include apt_update
+  include mysql
+}
+
+node 'web' {
+  include apt_update
+  include nginx
+  
+
+node 'dbserver' {
+  include apt_update
+  include mysql_server
+}
+
+
+node 'default' {
+include apt_get_update
+}
