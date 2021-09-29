6e14bd837b14118c79340eb3de275f3add035c72
Ahmed Ismail <54498216+A-Ismail-son@users.noreply.github.com>
2021-09-29 12:31:10 +0200

    Update mysql.pp

--- Basic Multi-Machine Setup/manifests/classes/mysql.pp
+++ Basic Multi-Machine Setup/manifests/classes/mysql.pp
@@ -1,1 +1,22 @@
 
+class mysql {
+  package { 'mysql-server':
+    ensure  => latest,
+    require => Exec['apt-get update']
+    
+    }
+    
+    service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server']
+    
+    }
+    
+    exec { 'mysqladmin':
+    command => '/usr/bin/mysqladmin -u root password PA2577',
+    require => Package['mysql-server'],
+  }
+}
+    
+    
+    
