f0e14c624a21a5970f90642c14963fc70150752e
Ahmed Ismail <54498216+A-Ismail-son@users.noreply.github.com>
2021-09-29 12:32:48 +0200

    Update nginx.pp

--- Basic Multi-Machine Setup/manifests/classes/nginx.pp
+++ Basic Multi-Machine Setup/manifests/classes/nginx.pp
@@ -1,1 +1,13 @@
 
+class nginx {
+
+  package { 'nginx':
+    ensure  => installed,
+    require => Exec['apt-get update']
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    require => Package['nginx']
+  }
+}
